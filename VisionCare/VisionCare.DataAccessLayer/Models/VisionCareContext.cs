02878017852abdb7ba271e8af174c3e5a3b2191c
nkhoa31 <nkhoa31103@gmail.com>
2026-04-18 00:21:31 +0700

    Merge -   resolve conflict, keep both OrderStatusHistory and PrescriptionValidationRule

--- VisionCare/VisionCare.DataAccessLayer/Models/VisionCareContext.cs
+++ VisionCare/VisionCare.DataAccessLayer/Models/VisionCareContext.cs
@@ -265,11 +265,8 @@ public partial class VisionCareContext : DbContext
                 .HasForeignKey(e => e.ChangedBy)
                 .HasConstraintName("FK__OrderStatus__User__7D6B9B87");
         });
-<<<<<<< HEAD
 
-=======
-        
->>>>>>> 85bf5ea4fa21feaa5fe3eebd6c508caa300ee8ae
+
         modelBuilder.Entity<PrescriptionValidationRule>(entity =>
         {
             entity.HasKey(e => e.RuleId).HasName("PK__PrescriptValidationRule");
@@ -280,10 +277,7 @@ public partial class VisionCareContext : DbContext
             entity.Property(e => e.MinValue).HasPrecision(10, 2);
             entity.Property(e => e.MaxValue).HasPrecision(10, 2);
             entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())").HasColumnType("datetime");
-<<<<<<< HEAD
             entity.Property(e => e.SortOrder).HasDefaultValue(0);
-=======
->>>>>>> 85bf5ea4fa21feaa5fe3eebd6c508caa300ee8ae
         });
 
         OnModelCreatingPartial(modelBuilder);
